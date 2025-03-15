6d073b7829de1396d170c76f7f5063b2b4b74092
jovlievs <jovliyevabbosjon@gmail.com>
2025-03-16 00:19:18 +0500

    deleted

--- str8.py
+++
@@ -1,5 +0,0 @@
-# 8. Write a program that asks the user for a string and prints the first and last characters of the string. 
-word = input("Enter a word: ")
-print(f"1st character: {{{word[0]}}} and last character: {{{word[-1]}}}")
-
-
