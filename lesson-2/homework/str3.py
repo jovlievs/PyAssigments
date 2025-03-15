a381619623f1975bd36a68fd07977b8cda314959
jovlievs <jovliyevabbosjon@gmail.com>
2025-03-15 14:42:19 +0500

    again simple one

---
+++ lesson-2/homework/str3.py
@@ -0,0 +1,6 @@
+# 3. Write a Python program to:
+#    - Take a string input from the user.
+#    - Print the length of the string.
+#    - Convert the string to uppercase and lowercase.
+word = input('Enter a word: ')
+print(f"The lenght of the word you entered: {len(word)} and {word.upper()} and {word.lower()}")
