4507ece04487788ba0baa153cfcb7219fc950f57
jovlievs <jovliyevabbosjon@gmail.com>
2025-03-15 21:30:28 +0500

    simple code

--- lesson-2/homework/number7.py
+++ lesson-2/homework/number7.py
@@ -1,3 +1,3 @@
 # # Create a program that takes a number and checks if it’s even or not
 number = int(input("Enter a number: "))
-print("Odd Even".split()[number & 1])
+print("Even Odd".split()[number % 2])
---
+++ lesson-2/homework/str5.py
@@ -0,0 +1,19 @@
+# from collections import Counter
+
+# word = input("Enter a word: ").lower()  # Convert to lowercase for uniformity
+# vowels = "aeiou"
+
+# # Count occurrences of characters
+# char_count = Counter(word)
+
+# # Count vowels using sum and filter
+# vowel_count = sum(map(char_count.get, filter(vowels.__contains__, word)))
+
+# # Total letters minus vowel count gives consonant count
+# consonant_count = sum(char_count.values()) - vowel_count  
+
+# print("Vowels:", vowel_count)
+# print("Consonants:", consonant_count)
+
+lst=input() 
+print(len(list(filter(lambda x: x in ['a','e','u','i','y','o'],lst))))
